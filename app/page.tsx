abf012d5e4211e2037783dd67aca110f5054e0d3
KYLE VANCE <vanceofalifetime@protonmail.com>
2024-10-31 13:08:04 -0700

    added next naviagtion to button

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,34 @@
 import { Button } from "@/components/ui/button";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { LoginButton } from "@/components/auth/login-button";
 
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 export default async function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-      <div className="space-y-6">
-        <h1 className={"text-6xl font-semibold text-white drop-shadow-md"}>
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
           🔒 Auth
         </h1>
-        <p className="text-white text-lg">
+        <p className="text-white text-lg text-center">
           A simple authentication service
         </p>
+        <div>
+          <LoginButton >
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
       </div>
     </main>
   );
