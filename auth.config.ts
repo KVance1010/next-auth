e55a12b79cdc1b3f5ee2264a424f3e6a1b810235
KYLE VANCE <vanceofalifetime@protonmail.com>
2024-11-07 16:37:53 -0800

    error on oath set up

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,22 @@
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
-import bcrypt from "bcryptjs";
 
 export default {
   providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
